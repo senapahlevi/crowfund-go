61cf18517abc947fa6c73cb401b4205df80ffeca
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2023-05-18 22:06:28 +0700

    add warming fundamental

--- main.go
+++ main.go
@@ -3,18 +3,52 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os/user"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// dsn := "root:@tcp(127.0.0.1:3306)/crowfund?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+
+	// }
+	// fmt.Println("good connect ya")
+
+	// var users []user.User
+
+	// db.Find(&users)
+	// for _, user := range users {
+
+	// 	fmt.Println("hello", user.Name)
+	// 	fmt.Println("hello email", user.Email)
+
+	// }
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowfund?charset=utf8mb4&parseTime=True&loc=Local"
 
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 
 	}
 	fmt.Println("good connect ya")
+
+	var users []user.User
+
+	db.Find(&users)
+	c.JSON(http.StatusOK, users)
+
 }
