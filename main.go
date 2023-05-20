19d351cb780bf31a967f5c67eb519fc5ac23ffcc
ezypolis.senapahlevi <sena.pahlevi@ezypolis.com>
2023-05-20 23:45:39 +0700

    repository user

--- main.go
+++ main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
+	"crowfund/user"
 	"fmt"
 	"log"
-	"net/http"
-	"os/user"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "root:@tcp(127.0.0.1:3306)/crowfund?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(127.0.0.1:3306)/crowfund?charset=utf8mb4&parseTime=True&loc=Local"
 
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// if err != nil {
-	// 	log.Fatal(err.Error())
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
 
-	// }
-	// fmt.Println("good connect ya")
+	}
+	fmt.Println("good connect ya")
 
 	// var users []user.User
 
@@ -30,25 +28,9 @@ func main() {
 	// 	fmt.Println("hello email", user.Email)
 
 	// }
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-
-}
-
-func handler(c *gin.Context) {
-	dsn := "root:@tcp(127.0.0.1:3306)/crowfund?charset=utf8mb4&parseTime=True&loc=Local"
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err.Error())
-
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "coba dari golang",
 	}
-	fmt.Println("good connect ya")
-
-	var users []user.User
-
-	db.Find(&users)
-	c.JSON(http.StatusOK, users)
-
+	userRepository.Save(user)
 }
---
+++ user/input.go
@@ -0,0 +1,8 @@
+package user
+
+// urutan
+// 	1input user input
+// 	2.handler hasil dari input akan dimapping ke struct
+// 	3.service mapping struct ke struct Model misal User
+// 	4.repository save ke struct ke db
+// 	5.db db mysql menerima dari golang
